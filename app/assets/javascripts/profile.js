1d0564b1a8251854c8fcd53cc0cea30b18fd28b8
Scott Jordan <sleepinghippy@hotmail.com>
2014-11-01 16:59:01 -0400

    Remove turbolinks.

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,6 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).on('page:change', function() {
+$(document).on('ready', function() {
     $('#editlink').click(function() {
         $('#endfooter').hide();
         $('#editfooter').fadeIn();
